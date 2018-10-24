60a061da8d095559c5bfc30fa54c2946eb0d1831
Stabmirz <amirza@techlaunch.io>
2018-10-24 05:43:54 -0400

    new commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 var button = document.querySelector('.start');
-var print = document.querySelector('.path');
-// var refButton = document.querySelector('.refresh')
+var print = document.querySelector('.matrix');
+var div = document.querySelector('.button');
+
 var preX = 0;
 var preY = 0;
 
@@ -38,8 +39,12 @@ array[28]= [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 array[29]= [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
-function clickButton(){
+function clickButton(){ 
+    button.style.visibility='hidden';
     print.innerHTML= array[0].join(" ")+'<br/>'+array[1].join(" ")+'<br/>'+array[2].join(" ")+'<br/>'+array[3].join(" ")+'<br/>'+array[4].join(" ")+'<br/>'+array[5].join(" ")+'<br/>'+array[6].join(" ")+'<br/>'+array[7].join(" ")+'<br/>'+array[8].join(" ")+'<br/>'+array[9].join(" ")+'<br/>'+array[10].join(" ")+'<br/>'+array[11].join(" ")+'<br/>'+array[12].join(" ")+'<br/>'+array[13].join(" ")+'<br/>'+array[14].join(" ")+'<br/>'+array[15].join(" ")+'<br/>'+array[16].join(" ")+'<br/>'+array[17].join(" ")+'<br/>'+array[18].join(" ")+'<br/>'+array[19].join(" ")+'<br/>'+array[20].join(" ")+'<br/>'+array[21].join(" ")+'<br/>'+array[22].join(" ")+'<br/>'+array[23].join(" ")+'<br/>'+array[24].join(" ")+'<br/>'+array[25].join(" ")+'<br/>'+array[26].join(" ")+'<br/>'+array[27].join(" ")+'<br/>'+array[28].join(" ")+'<br/>'+array[29].join(" ");
+    
+    // div.innerHTML = '<button id="refresh" style="padding: 0.5vw;color: white;font-weight: bold;background-image: linear-gradient(-225deg, #2B86C5 0%, #562B7C 52%, #FF3CAC 100%);">Refresh</button>';
+    // div.style.visibility = 'visible';
 
     setTimeout(() => {
         var posX =  15;
@@ -47,11 +52,12 @@ function clickButton(){
         array[preX][preY]= 0;
         posX +=Math.floor(Math.random()* 2);
         posY +=Math.floor(Math.random()* 2);
-        array[posX][posY] = 1;
+        array[posX][posY] = 'M';
         preX = posX;
         preY = posY;
         clickButton();
     }, 1000);
+    
 }
 
 
@@ -60,15 +66,5 @@ button.addEventListener('click',function(e){
 })
 
 // function myFunction(){
-//     var posX =  15;
-//     var posY = 15;
-//     array[preX][preY]= 1;
-//     posX+=Math.floor(Math.random()*2);
-//     posY+=Math.floor(Math.random()*2);
-//     array[posX][posY] = 0;
-//     preX = posX;
-//     preY = posY;
-
-//     clickButton();
+//     clickButton();    
 // }
-
