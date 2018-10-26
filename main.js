a1b7ca90fd0d3115bcc31d26ecb62eb95b7521e4
Stabmirz <amirza@techlaunch.io>
2018-10-25 20:59:51 -0400

    update

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ array[i] = new Array(30).fill( 0 );
 
 function clickButton(){ 
     button.style.visibility='hidden';
-    print.innerHTML= array[0].join(" &nbsp; ")+'<br/>'+array[1].join(" &nbsp; ")+'<br/>'+array[2].join(" &nbsp; ")+'<br/>'+array[3].join(" &nbsp; ")+'<br/>'+array[4].join(" &nbsp; ")+'<br/>'+array[5].join(" &nbsp; ")+'<br/>'+array[6].join(" &nbsp; ")+'<br/>'+array[7].join(" &nbsp; ")+'<br/>'+array[8].join(" &nbsp; ")+'<br/>'+array[9].join(" &nbsp; ")+'<br/>'+array[10].join(" &nbsp; ")+'<br/>'+array[11].join(" &nbsp; ")+'<br/>'+array[12].join(" &nbsp; ")+'<br/>'+array[13].join(" &nbsp; ")+'<br/>'+array[14].join(" &nbsp; ")+'<br/>'+array[15].join(" &nbsp; ")+'<br/>'+array[16].join(" &nbsp; ")+'<br/>'+array[17].join(" &nbsp; ")+'<br/>'+array[18].join(" &nbsp; ")+'<br/>'+array[19].join(" &nbsp; ")+'<br/>'+array[20].join(" &nbsp; ")+'<br/>'+array[21].join(" &nbsp; ")+'<br/>'+array[22].join(" &nbsp; ")+'<br/>'+array[23].join(" &nbsp; ")+'<br/>'+array[24].join(" &nbsp; ")+'<br/>'+array[25].join(" &nbsp; ")+'<br/>'+array[26].join(" &nbsp; ")+'<br/>'+array[27].join(" &nbsp; ")+'<br/>'+array[28].join(" &nbsp; ")+'<br/>'+array[29].join(" &nbsp; ");
-
+    print.innerHTML = creatTable();
+    
     // div.innerHTML = '<button id="refresh" style="padding: 0.5vw;color: white;font-weight: bold;background-image: linear-gradient(-225deg, #2B86C5 0%, #562B7C 52%, #FF3CAC 100%);">Refresh</button>';
     // div.style.visibility = 'visible';
 
@@ -31,7 +31,13 @@ function clickButton(){
     }, 1000);
     
 }
-
+function creatTable() {
+    var result = [];
+    for (var j = 0; j < 30; j++) {
+        result += array[j].join(" &nbsp; ") + '<br/>';
+    }
+    return result;
+}
 
 button.addEventListener('click',function(e){
     clickButton();
